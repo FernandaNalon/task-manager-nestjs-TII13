dd771bd5fe303ea0c7df478b75f1046df62411d8
FERNANDA NALON PIRES SAUAIA <fernanda.npsauaia@senacsp.edu.br>
2026-06-22 17:29:55 -0300

    feat: criação de uma aplicação que contém o CRUD em nestjs com consumo de banco de dados

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TarefasModule } from './tarefas/tarefas.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TarefasModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/tarefas/tarefas.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { connection } from '../database/database';
+import { CreateTarefaDto } from './dto/create-tarefa.dto';
+import { UpdateTarefaDto } from './dto/update-tarefa.dto';
+import { RowDataPacket } from 'mysql2'; 
+import { Tarefa } from './interfaces/tarefa.interface';
+
+@Injectable()
+export class TarefasService {
+
+    async listar() {
+        const [tarefa] = await connection.query('SELECT * FROM tarefa ORDER BY id DESC');
+        return tarefa;
+    }
+
+    async criar(createTarefaDto:CreateTarefaDto) {
+        const { titulo, descricao, status} = createTarefaDto;
+        await connection.query(
+        'INSERT INTO tarefa (titulo, descricao, status) VALUES (?,?,?)',
+        [titulo, descricao || '', status || 'pendente'],
+        );
+        return {
+            mensagem: 'Tarefa criada com sucesso!'
+        };
+    }
+
+}
